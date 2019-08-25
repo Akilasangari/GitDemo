4aa2e341410156f2a22e9f236f0f0ef704eee9b3
Akila <akilasangari@gmail.com>
2019-08-25 10:30:12 +1000

    first commit

---
+++ src/test/java/qaclickacademy/AppiumTest.java
@@ -0,0 +1,22 @@
+package qaclickacademy;
+
+import org.testng.annotations.Test;
+
+public class AppiumTest {
+	@Test
+	public void NativeAPI()
+	{
+		System.out.println("NativeAPI");
+	}
+	@Test
+	public void Mobileapp()
+	{
+		System.out.println("Mobileap");
+	}
+	@Test
+	public void IOSApp()
+	{
+		System.out.println("IOSApp");
+	}
+	
+}
---
+++ src/test/java/qaclickacademy/SeleniumTest.java
@@ -0,0 +1,19 @@
+package qaclickacademy;
+
+import org.testng.annotations.Test;
+
+public class SeleniumTest {
+
+	@Test
+	public void BrowserExe()
+	{
+		System.out.println("BrowserExe");
+		
+	}
+	@Test
+	public void ElementUI()
+	{
+		System.out.println(" ElementUI");
+		
+	}
+}
